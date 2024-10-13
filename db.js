87dbfcde907802821385b5cdd14866c49d99776e
Emms Kalifa <emman199810@gmail.com>
2024-10-13 21:50:23 +0800

    hard coded credentials

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise')
 // Create the connection pool. The pool-specific settings are the defaults
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port:process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password:process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'mysql-10c77f66-emman199810-8ec7.l.aivencloud.com',
+  port:'19417',
+  user: 'avnadmin',
+  password:'AVNS_6JwxnghvxWxLwO8P7bI',
+  database: 'defaultdb',
   waitForConnections: true,
   connectionLimit: 10,
   maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
